1b75b4ee69452faa11d3b1ea56e011bb60982438
Thaisllayne <thaisllaynesantana@gmail.com>
2021-07-05 15:28:26 -0300

    Renomeando a classe responsável por testar as operações básicas

--- src/TesteOperacoesBasicas.java
+++ src/TesteOperacoesBasicas.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Teste {
+public class TesteOperacoesBasicas {
     public static void main(String[] args) {
 
         Scanner leitor = new Scanner(System.in);
