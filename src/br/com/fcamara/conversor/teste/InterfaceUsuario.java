d42555b2ee2dca17a709dc4e03968a943bc22937
Thaisllayne <thaisllaynesantana@gmail.com>
2021-07-07 15:41:05 -0300

    Refactor: criação da classe InterfaceUsuario, para que o código fique mais limpo

---
+++ src/br/com/fcamara/conversor/teste/InterfaceUsuario.java
@@ -0,0 +1,84 @@
+package br.com.fcamara.conversor.teste;
+
+import br.com.fcamara.conversor.modelo.ConversorComprimento;
+import br.com.fcamara.conversor.modelo.ConversorMoeda;
+import br.com.fcamara.conversor.modelo.ConversorTemperatura;
+
+import java.util.Scanner;
+
+public class InterfaceUsuario {
+    private Scanner leitor;
+
+    public InterfaceUsuario() {
+        this.leitor = new Scanner(System.in);
+    }
+
+    public double forTemperatura(){
+        System.out.println("Informe qual a unidade de medida atual: \n" +
+                "1 - Celsius \n" +
+                "2 - Fahrenheit \n" +
+                "3 - Kelvin");
+        int escalaTermometricaAtual = leitor.nextInt();
+
+        System.out.println("Informe o valor da temperatura que deseja converter:");
+        int temperaturaAtual = leitor.nextInt();
+
+        System.out.println("Informe para qual unidade de medida será convertida: \n" +
+                "1 - Celsius \n" +
+                "2 - Fahrenheit \n" +
+                "3 - Kelvin");
+        int escalaTermometricaConvertida = leitor.nextInt();
+
+        ConversorTemperatura conversorTemperatura = new ConversorTemperatura(temperaturaAtual,
+                escalaTermometricaAtual, escalaTermometricaConvertida);
+        System.out.println(conversorTemperatura);
+
+        return escalaTermometricaConvertida;
+    }
+
+    public double forMoeda(){
+        System.out.println("Informe a moeda atual: \n" +
+                "1 - Dólar americano \n" +
+                "2 - Euro \n" +
+                "3 - Real");
+        int moedaAtual = leitor.nextInt();
+
+        System.out.println("Informe o valor atual:");
+        double valorMoedaAtual = leitor.nextDouble();
+
+        System.out.println("Informe para qual moeda será convertida: \n" +
+                "1 - Dólar americano \n" +
+                "2 - Euro \n" +
+                "3 - Real");
+        int moedaConvertida = leitor.nextInt();
+
+        ConversorMoeda conversorMoeda = new ConversorMoeda(valorMoedaAtual, moedaAtual, moedaConvertida);
+        System.out.println(conversorMoeda);
+
+        return valorMoedaAtual;
+    }
+
+    public double forComprimento(){
+
+        System.out.println("Informe a unidade de medida atual \n" +
+                "1 - Quilômetro \n" +
+                "2 - Metro \n" +
+                "3 - Centímetro");
+        int unidadeDeMedidaAtual = leitor.nextInt();
+
+        System.out.println("Informe o valor: ");
+        double valorMedidaAtual = leitor.nextDouble();
+
+        System.out.println("Informe para qual unidade de medida será convertida: \n" +
+                "1 - Quilômetro \n" +
+                "2 - Metro \n" +
+                "3 - Centímetro");
+        int unidadeDeMedidaConvertida = leitor.nextInt();
+
+        ConversorComprimento conversorComprimento = new ConversorComprimento(valorMedidaAtual,
+                unidadeDeMedidaAtual, unidadeDeMedidaConvertida);
+
+        return valorMedidaAtual;
+    }
+
+}
--- src/br/com/fcamara/conversor/teste/TesteConversor.java
+++ src/br/com/fcamara/conversor/teste/TesteConversor.java
@@ -10,85 +10,27 @@ public class TesteConversor {
     public static void main(String[] args) {
 
         Scanner leitor = new Scanner(System.in);
-
         System.out.println("Informe o tipo de conversão: \n" +
                 "1 - Temperatura \n" +
                 "2 - Moeda \n" +
                 "3 - Comprimento");
         int tipoConversao = leitor.nextInt();
 
+        InterfaceUsuario interfaceUsuario = new InterfaceUsuario();
+
         switch (tipoConversao){
             case 1:
-                System.out.println("Informe qual a unidade de medida atual: \n" +
-                        "1 - Celsius \n" +
-                        "2 - Fahrenheit \n" +
-                        "3 - Kelvin");
-                int escalaTermometricaAtual = leitor.nextInt();
-
-                System.out.println("Informe o valor da temperatura que deseja converter:");
-                int temperaturaAtual = leitor.nextInt();
-
-                System.out.println("Informe para qual unidade de medida será convertida: \n" +
-                        "1 - Celsius \n" +
-                        "2 - Fahrenheit \n" +
-                        "3 - Kelvin");
-                int escalaTermometricaConvertida = leitor.nextInt();
-
-                //System.out.println(temperaturaAtual);
-
-                ConversorTemperatura conversorTemperatura = new ConversorTemperatura(temperaturaAtual,
-                        escalaTermometricaAtual, escalaTermometricaConvertida);
-                System.out.println(conversorTemperatura);
+                interfaceUsuario.forTemperatura();
                 break;
-
             case 2:
-                System.out.println("Informe a moeda atual: \n" +
-                        "1 - Dólar americano \n" +
-                        "2 - Euro \n" +
-                        "3 - Real");
-                int moedaAtual = leitor.nextInt();
-
-                System.out.println("Informe o valor atual:");
-                double valorMoedaAtual = leitor.nextDouble();
-
-                System.out.println("Informe para qual moeda será convertida: \n" +
-                        "1 - Dólar americano \n" +
-                        "2 - Euro \n" +
-                        "3 - Real");
-                int moedaConvertida = leitor.nextInt();
-
-                ConversorMoeda conversorMoeda = new ConversorMoeda(valorMoedaAtual, moedaAtual, moedaConvertida);
-                System.out.println(conversorMoeda);
+                interfaceUsuario.forMoeda();
                 break;
-
             case 3:
-
-                System.out.println("Informe a unidade de medida atual \n" +
-                        "1 - Quilômetro \n" +
-                        "2 - Metro \n" +
-                        "3 - Centímetro");
-                int unidadeDeMedidaAtual = leitor.nextInt();
-
-                System.out.println("Informe o valor: ");
-                double valorMedidaAtual = leitor.nextDouble();
-
-                System.out.println("Informe para qual unidade de medida será convertida: \n" +
-                        "1 - Quilômetro \n" +
-                        "2 - Metro \n" +
-                        "3 - Centímetro");
-                int unidadeDeMedidaConvertida = leitor.nextInt();
-
-
-                ConversorComprimento conversorComprimento = new ConversorComprimento(valorMedidaAtual, unidadeDeMedidaAtual, unidadeDeMedidaConvertida);
-                System.out.println("entrei no conversor de comprimento");
+                interfaceUsuario.forComprimento();
                 break;
-
             default:
                 System.out.println("Por favor, informe um valor entre 1 e 3");
                 break;
         }
-
-
-
     }
 }
